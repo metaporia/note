f00378df942701443212b5da3d48aab7de0ac154
Keane Yahn-Krafft <thisaintforuruse@gmail.com>
2018-04-03 11:26:14 -0700

    tiny tweaks

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,33 +2,17 @@ module Lib
     ( someFunc
     ) where
 
-import qualified Data.Map as M
-
 import Prelude hiding (lookup)
+import Content 
+import Handle (Handle)
+import ContentMap
+
+import Data.Either
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
-type Id = Int
-type Blob = String
-
-data ContentMap = ContentMap (M.Map (Handle Id) (Content Id)) (Handle Id) deriving (Eq, Show)
-
-empty :: ContentMap
-empty = ContentMap M.empty (BlobId 0)
-
-insert :: Content Id -> ContentMap -> (ContentMap, Handle Id)
-insert b (ContentMap map nextId) =
-    ((ContentMap (M.insert nextId b map) $ succHandle nextId), nextId)
-
-lookup :: Handle Id -> ContentMap -> Maybe (Content Id)
-lookup id (ContentMap map _) = M.lookup id map
-
--- BlobId
-insertContents :: [Content Id] -> ContentMap -> (ContentMap, [(Handle Id)])
-insertContents bs map = foldr (\b (map', ids) -> let (map'', id) = insert b map' in
-                                                  (map'', (id:ids))) (map, []) bs
-
+-- test code
 b0 = "This is the first line\n"
 b1 = "This is the second line\n"
 b2 = "Guess which line this is: that which proceeds the second\n"
@@ -37,11 +21,6 @@ b2 = "Guess which line this is: that which proceeds the second\n"
 
 idList = IdList almostIdList0 :: Content Id
 
-data Content id = Blob String | IdList [Handle id] deriving (Eq, Ord, Show)
-data Handle id = BlobId id | IdListId id deriving (Eq, Ord, Show)
-succHandle :: Num id => Handle id -> Handle id
-succHandle (BlobId n) = BlobId (n + 1)
-succHandle (IdListId n) = IdListId (n + 1)
+(map'', lid) = insert idList map'
 
-
-
+(map''', llid) = insert (IdList [lid]) map''
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,22 @@
+module Spec where
+
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
+
+import Map (Handle(BlobId, IdListId))
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = test functor triggerHandle
+
+-- Handle
+
+instance Arbitrary id => Arbitrary (Handle id) where
+    arbitrary = oneof [BlobId <$> arbitrary, IdListId <$> arbitrary]
+
+instance Eq id => EqProp (Handle id) where (=-=) = eq
+
+triggerHandle = undefined :: Handle (Int, Int, Int)
+
+test testbatch trigger = quickBatch $ testbatch trigger
+
