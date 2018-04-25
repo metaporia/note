d57a94d18549bc338c50cd830df8c330937a12fa
metaporia <kyahnkrafft@me.com>
2018-04-25 11:22:39 -0700

    Editor API prototyping.

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,9 +3,13 @@
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE QuasiQuotes #-}
+
 module Lib
     ( someFunc
     ) where
+
+import Prelude hiding (init, lookup, insert, span)
 
 import qualified Data.Map as DM
 import Crypto.Hash --(SHA1, hash, hashWith)
@@ -16,7 +20,6 @@ import Data.ByteString.Conversion
 --import Content
 import Data.Maybe (fromJust, catMaybes)
 import Data.Monoid ((<>))
-import Prelude hiding (lookup, insert, span)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import Select
@@ -27,8 +30,10 @@ import Link
 import Data.Bifunctor
 import Val
 import Control.Monad.State
+import Control.Monad.State.Class
 import Control.Monad.Trans.Maybe
 import Data.Functor.Identity
+import Text.RawString.QQ
 
 
 someFunc :: IO ()
@@ -186,9 +191,68 @@ newtype MsT' s m a = MsT' { getMsT' :: StateT s (MaybeT m) a }
 
 -- | Loads the contents of some source file into a 'VMap'. Returns originial
 -- 'VMap' unchanged if the insertion fails.
+-- insert
 load :: (VMVal c, HashAlg alg) 
      => c
      -> VMap alg c
      -> (Maybe (Key alg), VMap alg c)
-load = undefined
+load c vmap = flipTuple $ insertRawBlob c vmap
 
+-- | 'load' with 'State' -- Should I: 
+--      i. make a transformer that /might/ work for multiple use-cases;
+--
+--     ii. write a product newtype which tuples up a 'VMap', a 'SelMap', and a
+--         'Links' (read "link map" if inconsistent nomenclature irks you) into
+--         one so that 'State' suffices for most high-level API calls as will
+--         occur (here) in 'Lib'.
+
+type VState alg c a = State (VMap alg c) a
+
+
+load' :: (VMVal c, HashAlg alg)
+      => c
+      -> VState alg c (Maybe (Key alg)) --State (VMap alg c) (Maybe (Key alg))
+load' = state . load
+
+-- | Like 'load\'', but loads content into empyt map. Use to initialize global
+-- state.
+init :: (VMVal c, HashAlg alg) => c -> VState alg c (Maybe (Key alg))
+init  c = StateT $ 
+    \s ->
+        runStateT (load' c) s
+
+spec = [r|
+The specificity of being perks up its over-anthropomorphized head with an
+expression approaching hopeful bewilderment, 'ooh, was that my name? do they
+wanna talk to me? ooh ooh.' Then it notices it's self imposed limits (having
+been made fully human to my mind, it is subject to the same conditions) and
+thinks to itself, 'why do I think 'why do I think 'why do I think 'why do I
+think '...ad infinitum''' until it notices the trend towards redundancy and has
+the presence of mind to deduce that as an entity bound by consciousness, and
+therefore perception as well, it must be a highly specific being comprised of
+equally specific parts. At this point the very-nearly-human representation of
+the specificity of being realizes that as a consciousness it is the product of
+itself, and as such, it's limits are a product of it's own limits and the whole
+she-bang is therefore self-inflicted (work that out. hah). Being human and
+conscious, Specificity (lets name him so as to fully indulge his
+anthropomorhpizaton) goes on to infer that his (lets further this metaphor with
+some binary gender) specificity, as one of the root causes of the self-imposed
+limitations upon his consciousness, is, in fact, an instance of a two generic
+phenomenon (specificity of being and consciousness) and upon noticing that he
+himself was title Specificity begins to wonder not only about the role his
+consciousness plays in the limitation of his consciousness (all things human
+comprise a subset of consciousness), but the role the specificity of being
+plays in the mechanic by which consciousness limits itself. He begins to
+exclaim "wait *I'm* Spec....". He's now, quite specifically dead. Not asleep or
+knocked unconscious, but very very dead. Much to absolutely nobody's chagrin,
+the cognitive overload of being doubly responsible for his predicament was too
+much for Specificity to handle.  Not that he ever had the chance. The
+subconscious of Specificity saw existential angst (try to concieve of the most
+pain imaginable. Now magnify it because both you and I lack the insight and
+experience to fathom such pain. We're too stupid) on a scale never beheld (that
+is, never beheld and *survived* ) and simply gave up, overriding what should
+have been Specificity's conscious decision to throw in the towel in the hopes
+of protecting the delicate symbiotic balance struck by the various forms of
+existential obfuscation -- it's a defense mechanism after all, except it killed
+him. What does that say about life?
+|]
